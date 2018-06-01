88ad7f49e06b3b6ddff3234de292468c0d52bc0d
Jocelyn Rodriguez <jrodriguez9205@hotmail.com>
2018-06-01 01:15:04 -0400

    cifrado en index.j y agregue comentarios

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-//Boton instrucciones
-const btn = document.getElementById("button-inst");
-const txt = document.getElementById ("puntos");//Texto detallado
-btn.addEventListener("click", function(){
+/*Aqui va mi boton de instrucciones*/
+const btn = document.getElementById("button-inst");//llama al boton de instrucciones
+const txt = document.getElementById ("puntos");//Instrucciones detalladas
+btn.addEventListener("click", function(){ //funcion para hacer el click en el boton
     txt.innerHTML = "<p> Aqui va detallada cada instruccion </p>";
-}) //Click del Boton e instrucciones
+}) //texto que aparece al hacer click en el botonm
 
-document.getElementById("cifra").addEventListener("click", () => {
-  let offset = document.getElementById("offset").value;
-  let message = document.getElementById("mensaje").value;
+/*Aqui funciona mi boton para cifrar*/
+document.getElementById("cifra").addEventListener("click", () => { //llama a cifrar y el click
+  let offset = document.getElementById("offset").value; //trae el offset segun su valor
+  let message = document.getElementById("mensaje").value; //trae el mensaje
   document.getElementById("result").innerHTML = window.cipher.encode(offset, message);
-});
+});//da el resultado cifrado segun el valor del offset selecionado
