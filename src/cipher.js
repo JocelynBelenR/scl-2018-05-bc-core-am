b23639074bef150c5dd5e593f43cc03ddf9e78e9
Jocelyn Rodriguez <jrodriguez9205@hotmail.com>
2018-06-01 01:51:24 -0400

    formula y comentarios cipher.js encode - comentarios index

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,20 @@
-window.cipher = {  // aca solo agregar parametros y no cambiar nada mas.
-  encode: () => {
-    /* Acá va tu código */
-  },
-  decode: () => {
-    /* Acá va tu código */
-  }
+window.cipher = {
+  //encode = para cifrar el texto
+  encode: (offset, str) => { //numero y texto - funcion
+    let mensaje = ''; // variable vacia por el mensaje que se va a escribir
+      offset = parseInt(offset); // guarda las letras ingresadas
+      for (let i = 0; i < str.length; i++) { //i en la posicion 0, i < a la longitug del str, i suma de 1 en 1
+        const letras = str.charCodeAt(i); // aqui va el codigo ascii
+          if (65 <= letras && letras <= 90) // reconoce mayusculas
+            mensaje += String.fromCharCode(((letras - 65 + offset) % 26) + 65); //formula para mayusculas del codigo ascii
+          else if (97 <= letras && letras <= 122) // reconoce minísculas 
+             mensaje += String.fromCharCode(((letras - 97 + offset) % 26) + 97); //formula para minusculas del codigo ascii
+          else
+            mensaje += str.charAt(i); //para concatenar las letas ingresadas en el mensaje
+      }//cierre for
+      return mensaje;//da el mensaje
+  }, //cierre encode
+
+  decode:
+
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 /*Aqui va mi boton de instrucciones*/
-const btn = document.getElementById("button-inst");//llama al boton de instrucciones
-const txt = document.getElementById ("puntos");//Instrucciones detalladas
+const btn = document.getElementById("button-inst");//const boton - llama al boton de instrucciones
+const txt = document.getElementById ("puntos");//const texto - llama a instrucciones detalladas
 btn.addEventListener("click", function(){ //funcion para hacer el click en el boton
     txt.innerHTML = "<p> Aqui va detallada cada instruccion </p>";
 }) //texto que aparece al hacer click en el botonm
@@ -8,6 +8,6 @@ btn.addEventListener("click", function(){ //funcion para hacer el click en el bo
 /*Aqui funciona mi boton para cifrar*/
 document.getElementById("cifra").addEventListener("click", () => { //llama a cifrar y el click
   let offset = document.getElementById("offset").value; //trae el offset segun su valor
-  let message = document.getElementById("mensaje").value; //trae el mensaje
-  document.getElementById("result").innerHTML = window.cipher.encode(offset, message);
+  let mensaje = document.getElementById("mensaje").value; //trae el mensaje
+  document.getElementById("result").innerHTML = window.cipher.encode(offset, mensaje);
 });//da el resultado cifrado segun el valor del offset selecionado
