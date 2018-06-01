4bea1bc0cb03fe87e87667f7a6c2f1307513bde4
Jocelyn Rodriguez <jrodriguez9205@hotmail.com>
2018-06-01 02:20:52 -0400

    correccion en archivos js y decore

--- src/cipher.js
+++ src/cipher.js
@@ -1,20 +1,22 @@
 window.cipher = {
   //encode = para cifrar el texto
   encode: (offset, str) => { //numero y texto - funcion
-    let mensaje = ''; // variable vacia por el mensaje que se va a escribir
+      let message = ''; // variable vacia por el mensaje que se va a escribir
       offset = parseInt(offset); // guarda las letras ingresadas
       for (let i = 0; i < str.length; i++) { //i en la posicion 0, i < a la longitug del str, i suma de 1 en 1
-        const letras = str.charCodeAt(i); // aqui va el codigo ascii
-          if (65 <= letras && letras <= 90) // reconoce mayusculas
-            mensaje += String.fromCharCode(((letras - 65 + offset) % 26) + 65); //formula para mayusculas del codigo ascii
-          else if (97 <= letras && letras <= 122) // reconoce minísculas 
-             mensaje += String.fromCharCode(((letras - 97 + offset) % 26) + 97); //formula para minusculas del codigo ascii
+          const character = str.charCodeAt(i); // aqui va el codigo ascii
+          if (65 <= character && character <= 90) // para identificar las mayúsculas
+              message += String.fromCharCode(((character - 65 + offset) % 26) + 65); //formula
+          else if (97 <= character && character <= 122) // para identificar las minísculas 
+              message += String.fromCharCode(((character - 97 + offset) % 26) + 97); //formula para minusculas del codigo ascii
           else
-            mensaje += str.charAt(i); //para concatenar las letas ingresadas en el mensaje
-      }//cierre for
-      return mensaje;//da el mensaje
-  }, //cierre encode
-
-  decode:
+              message += str.charAt(i); //para concatenar las letas
+      } //cierre for
+      return message; //da el mesaje
+  },
+  //numero y texto - funcion  //forma y resultado segun los datos anteriores
+  decode: (offset, str) => { 
+      return window.cipher.encode((26 - offset), str);
+  }
 
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-/*Aqui va mi boton de instrucciones*/
-const btn = document.getElementById("button-inst");//const boton - llama al boton de instrucciones
-const txt = document.getElementById ("puntos");//const texto - llama a instrucciones detalladas
-btn.addEventListener("click", function(){ //funcion para hacer el click en el boton
+//Boton instrucciones
+var btn = document.getElementById("button-inst");
+var txt = document.getElementById ("puntos");//Texto detallado
+btn.addEventListener('click', function(){
     txt.innerHTML = "<p> Aqui va detallada cada instruccion </p>";
-}) //texto que aparece al hacer click en el botonm
+}) //Click del Boton e instrucciones
 
-/*Aqui funciona mi boton para cifrar*/
-document.getElementById("cifra").addEventListener("click", () => { //llama a cifrar y el click
-  let offset = document.getElementById("offset").value; //trae el offset segun su valor
-  let mensaje = document.getElementById("mensaje").value; //trae el mensaje
-  document.getElementById("result").innerHTML = window.cipher.encode(offset, mensaje);
-});//da el resultado cifrado segun el valor del offset selecionado
+document.getElementById("encode").addEventListener("click", () => {
+  let offset = document.getElementById('offset').value;
+  let message = document.getElementById('mensaje').value;
+  document.getElementById('result').innerHTML = window.cipher.encode(offset, message);
+});
+document.getElementById("decode").addEventListener("click", () => {
+  let offset = document.getElementById('offset').value;
+  let message = document.getElementById('mensaje').value;
+  document.getElementById('result').innerHTML = window.cipher.decode(offset, message);
+});
