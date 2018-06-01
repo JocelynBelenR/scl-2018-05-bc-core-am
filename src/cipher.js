3345a176bc72a3958416496dc82057a5fd02a8f4
Jocelyn Rodriguez <jrodriguez9205@hotmail.com>
2018-06-01 00:51:57 -0400

    cambios de orden en html para entender y realizar funcionamiento

--- src/cipher.js
+++ src/cipher.js
@@ -1,23 +1,3 @@
-window.cipher = {  // aca solo agregar parametros y no cambiar nada mas.
-  encode: () => {	  
-    /* Acá va tu código */
-  },	   
-  decode: () => {	  
-    /* Acá va tu código */	
-  }	  
-
-/*let abc = ['ABCDEFGHIJKLMNÑOPQRSTUVWXYZ']; letras en su valor original del 0-26/*
-  for (let i = 0; i< abc.length; i++) {
-  console.log(abc[i]);
+window.cipher = {
+ 
 }
-
-
-/* Pasos logicos:
- leer mensaje, 
- leer cada letra segun posicion (del 0 al... con el for),
- letra leida se le saca el codigo ac chartcodeat, 
- da un numero entre 65-90, 
- al numero aplicar ecuacion (aqui se ocupa offset= cuando se mueve)
- la ecuacion da un nuevo numero y se le hace stringchatcode
- esto da una letra: la nueva letra en ubicacion cesar
- con esa lee el mensaje y hace esto con cada letra del mensaje{ */}
--- src/index.js
+++ src/index.js
@@ -1,51 +1,7 @@
-window.onload = function () {
-    //Boton instrucciones
-  const btn = document.getElementById("button-inst");
-  const txt = document.getElementById ("puntos");//Texto detallado
-  btn.addEventListener('click', function(){
-  txt.innerHTML = "<p> Aqui va detallada cada instruccion </p>";
-  }) //Click del Boton e instrucciones 
-
-  const numero = document.getElementById("password"); /*llamar al cuadro donde ingreso el numero*/
-  const strboton = document.getElementById("enviarUno"); /*llamar al boton de enviar para el primer texto*/
-
-
-}  
-
-
-    
-
-
-
-
-
-/*function cesar (str) {
-    var minusculas = str.toLowerCase ();
-    var abc = 'abcdefghijklmnñopqrstuvwxyz';
-    var newStr = ' ';
-
-    for(var i=0 < minusculas.length; i++;) {
-        var movimiento = minusculas[i];
-        if (movimiento === ' ') {
-            newStr += movimiento;
-            continue;
-
-        }
-    }
-}*/
-
-/*function cifrar (inputUno) {
-
-document.getElementById(textoCifrado);
-}*/
-
-/*(x+n)%26 = (posicion de la letra - valor fijo) sacar el residuo de %26
-Ejemplo con A
-
-(x-65+n)%26+65 ...n°letra ascii-ubicacion ascii+valor fijo(offset)%26 + 65 (x =declarar esa variable)
-
- ("a","b","c","d","e","f","g","h","i","j","k","l","m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z") 
-*/
-
-
+//Boton instrucciones
+var btn = document.getElementById("button-inst");
+var txt = document.getElementById ("puntos");//Texto detallado
+btn.addEventListener('click', function(){
+    txt.innerHTML = "<p> Aqui va detallada cada instruccion </p>";
+}) //Click del Boton e instrucciones
 
